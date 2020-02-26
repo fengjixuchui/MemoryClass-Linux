0dbca55f53c8edf3ada68dde83ba761f78e99446
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-02-26 16:57:50 -0300

    Add files via upload

---
+++ memory_class/memory.cpp
@@ -0,0 +1,59 @@
+#include "memory.h"
+
+int Mem::GetProcessID(std::string processName)
+{
+    int pid = INVALID_PID;
+    DIR* pdir = opendir("/proc");
+    if(!pdir)
+        return INVALID_PID;
+
+    struct dirent* pdirent;
+    while(pid < 0 && (pdirent = readdir(pdir)))
+    {
+        int id = atoi(pdirent->d_name);
+        if(id > 0)
+        {
+            std::string cmdpath = std::string("/proc/") + pdirent->d_name + "/cmdline";
+            std::ifstream cmdfile(cmdpath.c_str());
+            std::string cmdline;
+            getline(cmdfile, cmdline);
+            size_t pos = cmdline.find('\0');
+            if(!cmdline.empty())
+            {
+                if (pos != std::string::npos)
+                    cmdline = cmdline.substr(0, pos);
+                pos = cmdline.rfind('/');
+                if (pos != std::string::npos)
+                    cmdline = cmdline.substr(pos + 1);
+                if (processName == cmdline.c_str())
+                    pid = id;
+            }
+        }
+    }
+    closedir(pdir);
+    return pid;
+}
+
+void Mem::ReadBuffer(int pid, off_t address, void* buffer, size_t size)
+{
+    char file[MAX_FILENAME];
+    sprintf(file, "/proc/%ld/mem", (long)pid);
+    int fd = open(file, O_RDWR);
+    ptrace(PTRACE_ATTACH, pid, 0, 0);
+    waitpid(pid, NULL, 0);
+    pread(fd, buffer, size, address);
+    ptrace(PTRACE_DETACH, pid, 0, 0);
+    close(fd);
+}
+
+void Mem::WriteBuffer(int pid, off_t address, void* value, size_t size)
+{
+    char file[MAX_FILENAME];
+    sprintf(file, "/proc/%ld/mem", (long)pid);
+    int fd = open(file, O_RDWR);
+    ptrace(PTRACE_ATTACH, pid, 0, 0);
+    waitpid(pid, NULL, 0);
+    pwrite(fd, value, size, address);
+    ptrace(PTRACE_DETACH, pid, 0, 0);
+    close(fd);
+}
---
+++ memory_class/memory.h
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#define INVALID_PID -1
+#define INVALID_MEMFILE -1
+#define MAX_FILENAME 256
+
+namespace Mem
+{
+    int GetProcessID(std::string processName);
+    void ReadBuffer(int pid, off_t address, void* buffer, size_t size);
+    void WriteBuffer(int pid, off_t address, void* value, size_t size);
+}
